de73b4fcd1c210960304ac8d389d404f4100c471
ken0426 <kenkaitixyou0426@gmail.com>
2022-02-15 23:06:51 +0900

    useStateの更新。不具合あったので修正。

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('ダイヤン');
+  const [hobby, setHobby] = useState('');
+
+  const onPressButton = () => {
+    setName('田中');
+  }
+
+  const onChangeText = (value) => {
+    setHobby(value);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={hobby}
+      />
+      <Text>私の名前は{name}です</Text>
+      <Text>趣味は{hobby}です</Text>
+      <Button title='名前を変える' onPress={onPressButton} ></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +34,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
