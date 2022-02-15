ce9538be63dcb0bd94cfaa98d730ceef200eea3f
ken0426 <kenkaitixyou0426@gmail.com>
2022-02-15 23:30:08 +0900

    ループ処理の復習

--- App.js
+++ App.js
@@ -1,28 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity, Alert } from 'react-native';
+import { YellowBox } from 'react-native-web';
 
 export default function App() {
-  const [name, setName] = useState('ダイヤン');
-  const [hobby, setHobby] = useState('');
 
-  const onPressButton = () => {
-    setName('田中');
-  }
+  const names = [
+    {name: 'Aさん', key: 1},
+    {name: 'Bさん', key: 2},
+    {name: 'Cさん', key: 3},
+    {name: 'Dさん', key: 4},
+    {name: 'Eさん', key: 5},
+    {name: 'Fさん', key: 6},
+    {name: 'Gさん', key: 7},
+    {name: 'Hさん', key: 8},
+    {name: 'Iさん', key: 9},
+    {name: 'Aさん', key: 10},
+    {name: 'Bさん', key: 11},
+    {name: 'Cさん', key: 12},
+    {name: 'Dさん', key: 13},
+    {name: 'Eさん', key: 14},
+    {name: 'Fさん', key: 15},
+    {name: 'Gさん', key: 16},
+    {name: 'Hさん', key: 17},
+    {name: 'Iさん', key: 18},
+  ]
 
-  const onChangeText = (value) => {
-    setHobby(value);
-  }
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={hobby}
+      <FlatList
+        data={names}
+        renderItem={({item})=> (
+          <TouchableOpacity onPress={() => Alert.alert('名前', item.name)} key={item.key} style={styles.item}>
+            <Text style={styles.name}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
       />
-      <Text>私の名前は{name}です</Text>
-      <Text>趣味は{hobby}です</Text>
-      <Button title='名前を変える' onPress={onPressButton} ></Button>
+      {/* <ScrollView>
+      {names.map((item) => (
+        <View key={item.key} style={styles.item}>
+          <Text style={styles.name}>{item.name}</Text>
+        </View>
+      ))}
+      </ScrollView> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -35,10 +55,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+  item:{
+    width:300,
+    marginBottom:30,
+    backgroundColor:'yellow',
   },
+  name:{
+    fontSize:30,
+    textAlign:'center',
+  }
 });
