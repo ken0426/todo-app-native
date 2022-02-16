999dc76ec70439396177eb0957b132e71733f137
ken0426 <kenkaitixyou0426@gmail.com>
2022-02-16 23:54:03 +0900

    不要なAlertを削除

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity, Alert, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, TextInput, Button } from 'react-native';
 
 export default function App() {
   const [text, setText] = useState('')
